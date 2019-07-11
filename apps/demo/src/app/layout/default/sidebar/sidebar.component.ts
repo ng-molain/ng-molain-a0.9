7c4b8716c094868c16536c1ce26f49da818bca64
Tony Een <tonyeen@foxmail.com>
2019-07-12 00:04:50 +0800

    side nav

--- apps/demo/src/app/layout/default/sidebar/sidebar.component.ts
+++ apps/demo/src/app/layout/default/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApplicationContext } from '@ng-molain/demo/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'demo-layout-sidebar',
@@ -13,10 +14,36 @@ export class LayoutSidebarComponent implements OnInit {
   constructor(
     private applicationContext: ApplicationContext,
   ) {
-    this.menus = applicationContext.systemInfo.system.menu;
+    const menus = _.cloneDeep(applicationContext.systemInfo.menu);
+    visit(menus, (it, level) => {
+      it.name = it.text;
+      it.route = it.link;
+      it.level = level;
+      it.type = it.group ? 'group' : (_.has(it, 'children') ? 'submenu' : 'menuitem')
+    });
+    this.menus = menus;
   }
 
   ngOnInit() {
   }
 
+}
+
+function visit(nodes: any[], callback: (node: any, level: number) => void = () => {}, level: number = 0, deepInBy: string = 'children'){
+  const forIn = (_nodes: any[], _level: number = level) => {
+    if (!_.isArray(_nodes) || _.isEmpty(_nodes) || !_.isFunction(callback)) {
+      return ;
+    }
+
+    _nodes.forEach(node => {
+      callback(node, _level);
+
+      if (!!deepInBy && _.has(node, deepInBy)) {
+        const children = _.get(node, deepInBy);
+        forIn(children, _level + 1);
+      }
+    });
+  }
+
+  forIn(nodes);
 }
