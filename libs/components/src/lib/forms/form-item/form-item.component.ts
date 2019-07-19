ebc13b7713d76c9827f48a67553598e200ff908a
Tony <tonyeen@foxmail.com>
2019-07-19 09:25:15 +0800

    add forms component

---
+++ libs/components/src/lib/forms/form-item/form-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ml-form-item',
+  templateUrl: './form-item.component.html',
+  styleUrls: ['./form-item.component.scss']
+})
+export class FormItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/components/src/lib/forms/forms.module.ts
+++ libs/components/src/lib/forms/forms.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormItemComponent } from './form-item/form-item.component';
+import { FieldsetComponent } from './fieldset/fieldset.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FormItemComponent, FieldsetComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [FormItemComponent, FieldsetComponent]
 })
 export class FormsModule { }
