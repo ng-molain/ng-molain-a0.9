6032920c317b7c7cdd1b81a94e081296e916ea81
Tony Een <tonyeen@foxmail.com>
2019-07-20 15:36:08 +0800

    fix demo header

--- apps/demo/src/app/layout/default/header/header.component.ts
+++ apps/demo/src/app/layout/default/header/header.component.ts
@@ -8,6 +8,12 @@ import { DefaultLayoutService } from '../../default-layout.service';
 })
 export class LayoutHeaderComponent implements OnInit {
 
+  sites = [
+    {label: '根站点', value: 'root-site'},
+    {label: '子站点A', value: 'sub-site-a'},
+    {label: '子站点B', value: 'sub-site-b'},
+  ];
+
   constructor(public readonly layoutService: DefaultLayoutService) { }
 
   ngOnInit() {
