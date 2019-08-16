a23f4c5871ed45bdcc9ea24ddf6401141146d3c8
Tony Een <tonyeen@foxmail.com>
2019-08-16 11:26:11 +0800

    update publish script

--- scripts/publish/libs.publish.sh
+++ scripts/publish/libs.publish.sh
@@ -18,25 +18,24 @@ for ARG in "$@"; do
 done
 echo "Use snapshot mode ${SNAPSHOT}"
 
-VERSION=$(node -p "require('./package.json').version")
-echo "Prev version ${VERSION}"
+# VERSION=$(node -p "require('./package.json').version")
+# echo "Prev version ${VERSION}"
 
 # 1. patch version and git tag
 # npm version 0.8.0-beta.7 -m "Release version %s"
-# VERSION=$(node -p "require('./package.json').version")
-# echo "Current version ${VERSION}"
+VERSION=$(node -p "require('./package.json').version")
+echo "Current version ${VERSION}"
 
 # 2. build libs
 # npm run libs:build
 
 # 3. fix libs package versions
 # VERSION=0.8.0-beta.05
-echo "Fix all libs version with '${VERSION}'"
+echo "Fix all libs version with ${VERSION}"
 
 fixLibsVersion() {
     (cd ${ROOT}; for p in `ls .`; do sed -i "s/0\.0\.0-PLACEHOLDER/${VERSION}/g" ${p}/package.json; done)
 }
-# echo "Fixed all libs version to ${VERSION}"
 fixLibsVersion
 
 # 4. publish on npm.parim.net
